af301b2a2fffe3400090d9d18982b30566e6e600
audovychenko-mentoring <udovichenkoanastasiia@gmail.com>
2020-09-11 01:22:04 -0400

    run with different attributes from command line

---
+++ BaseTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitParametrizedTestProject
+{
+    public class BaseTest
+    {
+        private Dictionary<Environment, string> PossibleEnvironments
+        {
+            get
+            {
+                return new Dictionary<Environment, string>()
+                {
+                    { Environment.QA, Settings.QAUrl },
+                    { Environment.Production, Settings.ProductionUrl },
+                    { Environment.Development, Settings.DevelopmentUrl},
+                };
+            }
+        }
+
+        public Environment CurrentEnvironment { get; set; }
+
+        protected string CurrentEnvironmentURL
+        {
+            get 
+            {
+                string url;
+                if (PossibleEnvironments.TryGetValue(CurrentEnvironment, out url))
+                {
+                    return url;
+                }
+                throw new InvalidOperationException(string.Format("({0}) is not valid", CurrentEnvironment));
+            }
+        }
+
+        public BaseTest(Environment environment)
+        {
+            CurrentEnvironment = environment;
+        }
+
+        public BaseTest()
+        { 
+        }
+    } 
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,21 +1,28 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
 
 namespace NUnitParametrizedTestProject
 {
-    public class Tests
+    [TestFixture(Environment.Development)]
+    public class LoginTest : BaseTest
     {
-        [SetUp]
-        public void Setup()
+        public LoginTest(Environment currentEnvironment)
         {
+            CurrentEnvironment = currentEnvironment;
         }
 
         [Test]
         public void Test1()
         {
             IWebDriver driver = new ChromeDriver("C:\\Program Files\\WebDrivers");
-            driver.Navigate().GoToUrl("https://www.facebook.com");
+            if (TestContext.Parameters["environment"] != null)
+            {
+                Enum.TryParse(TestContext.Parameters["environment"], out Environment environment);
+                CurrentEnvironment = environment;
+            }
+            driver.Navigate().GoToUrl(CurrentEnvironmentURL);
         }
     }
 }
