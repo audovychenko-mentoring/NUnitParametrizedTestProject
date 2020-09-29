ea818f2c33a3963986d053b5505bdfa2f8c641eb
audovychenko-mentoring <udovichenkoanastasiia@gmail.com>
2020-09-29 16:25:15 -0400

    Selenium grid

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
 using System;
 
 namespace NUnitParametrizedTestProject
@@ -16,13 +18,18 @@ namespace NUnitParametrizedTestProject
         [Test]
         public void Test1()
         {
-            IWebDriver driver = new ChromeDriver("C:\\Program Files\\WebDrivers");
+            //IWebDriver driver = new ChromeDriver("C:\\Program Files\\WebDrivers");
+            IWebDriver driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), new FirefoxOptions().ToCapabilities());
             if (TestContext.Parameters["environment"] != null)
             {
                 Enum.TryParse(TestContext.Parameters["environment"], out Environment environment);
                 CurrentEnvironment = environment;
             }
             driver.Navigate().GoToUrl(CurrentEnvironmentURL);
+            
+            IWebDriver driver2 = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), new ChromeOptions().ToCapabilities());
+            driver2.Navigate().GoToUrl(CurrentEnvironmentURL);
+
         }
     }
 }
---
+++ UnitTest2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Safari;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Remote;
+using NUnit;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace GridTest
+{
+    [TestFixture]
+    [Parallelizable]
+    public class Test_Grid
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            var capabilities = new DesiredCapabilities("internet explorer", string.Empty, new Platform(PlatformType.Any));
+            capabilities.SetCapability("ignoreProtectedModeSettings", true);
+
+            driver = new RemoteWebDriver(new Uri("http://localhost:4445/wd/hub"), capabilities);
+        }
+
+        [Test]
+        [Parallelizable]
+        public void GoogleSearch()
+        {
+            string homepage = "http://www.google.com";
+            driver.Navigate().GoToUrl(homepage);
+
+        }
+
+        [Test]
+        [Parallelizable]
+        public void BingSearch()
+        {
+            driver.Navigate().GoToUrl("http://www.bing.com");
+        }
+
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+    }
+}
